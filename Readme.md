f6c0d7120aff05afb0999d3704ee3edeb2d90568
Maxwel Dsouza <maxellusionist@gmail.com>
2015-06-03 17:57:18 +0530

    fix crud page for new schema

--- Readme.md
+++ Readme.md
@@ -1,24 +1,5 @@
 JsSeo
 =====
 
-DONE
-unicode support
-
-TODO
-list links
-
-automatic sitemap
-remove cookies
-remove script tags
-dont load images
-respect robots.txt
-handler http errors
-cache api
-cache freshness
-headless browser
-mobile bot
-compression
-only process text/html
-block calls to ad services, analytics etc..
-escaped fragment support
-support cannonical, mobile site etc
+A way to render web pages that are generated on the client
+side for web robots.
--- main.py
+++ main.py
@@ -6,39 +6,55 @@ import hashlib
 import config
 import mysqldbhelper
 from urlparse import urlparse
+import datetime
 
 db = mysqldbhelper.DatabaseConnection(config.hostname,
                     user=config.user,
                     passwd=config.passwd,
                     db=config.db)
 
+json_output = mysqldbhelper.json_output
+
 def remove_script_tags(content):
     #TODO
     return content
 
+def parse_url(url):
+    class UrlObj():
+        pass
+    result = UrlObj()
+    urlobj = urlparse(url)
+    origin = urlobj.scheme + '://' + urlobj.netloc
+    path = url[len(origin):]
+    result.origin = origin
+    result.path = path
+    return result
+
 class ApiHandler(tornado.web.RequestHandler):
     def get(self, path):
+        self.set_header('Access-Control-Allow-Origin', '*')
+        self.set_header('Content-Type', 'application/json')
         action = self.get_argument('action')
         hostname = self.get_argument('hostname')
-        if action == 'list-urls':
-            limit = self.get_argument('limit', 20)
-            offset = self.get_argument('offset', 0)
-            #TODO
-            if limit > 100 or limit <= 0:
-                self.write('limit is out of range')
-            if offset < 0:
-                self.write('offset is out of range')
-            urls = db.get_all('''
-            select url_path
-            from url
-            where site_hostname = %s''', (hostname))
+        if action == 'next-path':
+            #limit = int(self.get_argument('limit', 20))
+            #offset = int(self.get_argument('offset', 0))
+            #if limit > 100 or limit <= 0:
+            #    self.write('limit is out of range')
+            #if offset < 0:
+            #    self.write('offset is out of range')
+            url = db.get_one('''
+            select page_path
+            from page
+            where site_hostname = %s''', (hostname,))
             result = {
                     'hostname': hostname,
-                    'urls': [x[0] for x in urls]
+                    'next-path': url
                     }
-            self.write(mysqldbhelper.json_output(result))
+            self.write(json_output(result))
 
     def post(self, path):
+        self.set_header('Access-Control-Allow-Origin', '*')
         action = self.get_argument('action')
         hostname = self.get_argument('hostname')
         path = self.get_argument('path')
@@ -55,57 +71,76 @@ class ApiHandler(tornado.web.RequestHandler):
             site_hostname = %s''', (path, hostname))
 
 class PageHandler(tornado.web.RequestHandler):
-    def get(self, path):
-        url = urlparse(path)
+    def get(self, url):
+        self.set_header('Content-Type', 'text/html')
+        url = parse_url(url)
         content = db.get_one('''
         select page_content from page
-        where page_url = %s
-        ''', (path,))
+        where page_path = %s
+        ''', (url.path,))
         self.write(content)
 
-    def post(self, path):
+    def post(self, url):
         try:
+            self.set_header('Content-Type', 'application/json')
+
             content = self.get_argument('content')
             content = remove_script_tags(content)
             content = content.encode('utf-8')
             hsh = hashlib.sha1(content).hexdigest()
 
-            urlobj = urlparse(path)
-            print urlobj
-            hostname = urlobj.netloc
-            scheme = urlobj.scheme
-            self.set_header('Access-Control-Allow-Origin', scheme + '://' + hostname)
+            urlobj = parse_url(url)
+            self.set_header('Access-Control-Allow-Origin', urlobj.origin)
 
-            oldhash = db.get_one('''
-            select page_sha1 from page
-            where page_url = %s''', (path,))
+            current = db.get_one('''
+            select page_id, page_sha1 from page
+            where
+            site_hostname = %s and
+            page_path = %s''', (urlobj.origin, urlobj.path))
 
-            if not oldhash:
+            expires = datetime.datetime.now() + datetime.timedelta(0, 86400) #secs
+
+            if not current:
                 db.put('''
                 insert into page
-                (page_hostname, page_url, page_content, page_sha1) values
-                (%s, %s, %s, %s)
-                ''', (hostname, path, content, hsh))
-            if hsh == oldhash:
-                self.write('page not changed')
+                (site_hostname, page_path, page_content, page_sha1, page_expires) values
+                (%s, %s, %s, %s, %s)
+                ''', (urlobj.origin, urlobj.path, content, hsh, expires))
+                self.write(json_output({
+                    'message': 'successfully created'
+                    }))
+
             else:
+                pageid = current[0]
                 db.put('''
                 update page
                 set
-                page_hostname = %s, page_content = %s, page_sha1 = %s
+                site_hostname = %s, page_content = %s, page_sha1 = %s,
+                page_expires = %s
                 where
-                page_url = %s
-                ''', (hostname, content, hsh, path))
+                page_id = %s
+                ''', (urlobj.origin, content, hsh, pageid, expires))
+                self.write(json_output({
+                    'message': 'successfully updated'
+                    }))
 
         except Exception, e:
             self.set_status(500)
             print str(e)
-            self.write(str(e))
+            self.write(json_output({
+                'message': str(e)
+                }))
 
-    def delete(self, path):
+    def delete(self, url):
+        urlobj = parse_url(url)
         db.put('''
         delete from page
-        where page_url = %s''', (path,))
+        where page_path = %s
+        and
+        site_hostname = %s''', (urlobj.path, urlobj.origin))
+        self.write(json_output({
+            'message': 'successfully deleted'
+            }))
 
 settings = {
     #'default_handler_class': ErrorHandler,
