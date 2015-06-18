b5779af72e1df8998691bc84b62b1c1ce36c1d28
Maxwel Dsouza <maxellusionist@gmail.com>
2015-06-18 18:39:33 +0530

    support for chromium browser

--- browser.py
+++ browser.py
@@ -6,6 +6,8 @@ import signal
 '''for starting chrome with a custom user agent
 chrome processes should not be running already'''
 
+botagent = 'jsSeoBot'
+
 def kill_process(pid):
     try:
         os.kill(int(pid), signal.SIGTERM)
@@ -13,35 +15,47 @@ def kill_process(pid):
         print str(ex)
         os.kill(int(pid), signal.SIGKILL)
 
-def start_chrome(url):
-    '''run a browser in a subprocess'''
-    return subprocess.Popen(['google-chrome', '--user-agent=jsSeoBot', url])
-
-def test():
-    chrome = start_chrome()
-    time.sleep(20)
-    kill_process(chrome.pid)
-
-def chrome_running_instances():
+def running_instances(browser):
     '''returns a list of pids of running chrome processes'''
     try:
-        pids = subprocess.check_output(["pidof", "chrome"])
+        pids = subprocess.check_output(["pidof", browser])
         pids = pids.split(' ')
         return pids
 
     except subprocess.CalledProcessError:
         return []
 
-def kill_chrome_instances():
+def kill_instances(pids):
     '''checks for running google-chrome instances every 5 seconds.
     kills all of them in turn. returns after all instances have
     closed'''
     while True:
         time.sleep(5)
-        for pid in chrome_running_instances():
+        for pid in pids:
             kill_process(pid)
         else:
             break
 
-kill_chrome_instances()
-start_chrome('http://localhost')
+# Chrome
+def start_chrome(url):
+    '''run a browser in a subprocess'''
+    return subprocess.Popen(['google-chrome', '--user-agent=' + botagent, url])
+
+def chrome_running_instances():
+    return running_instances("chrome")
+
+def kill_chrome_instances():
+    kill_instances(chrome_running_instances())
+
+# Chromium
+def start_chromium(url):
+    return subprocess.Popen(['chromium-browser', '--user-agent=' + botagent, url])
+
+def chromium_running_instances():
+    return running_instances("chromium-browser")
+
+def kill_chromium_instances():
+    kill_instances(chromium_running_instances())
+
+kill_chromium_instances()
+start_chromium('http://localhost')
