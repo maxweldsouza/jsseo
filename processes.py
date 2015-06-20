ce3a948d73230578c9229db0621ea8fba2105ff8
Maxwel Dsouza <maxellusionist@gmail.com>
2015-06-20 21:54:36 +0530

    minor fixes

--- processes.py
+++ processes.py
@@ -2,6 +2,18 @@ import os
 import time
 import subprocess
 import signal
+import logging
+from logging.handlers import RotatingFileHandler
+
+# setup logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger('jsSeo')
+
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler = RotatingFileHandler('jsSeo.log', maxBytes=10*1000*1000, backupCount=5)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 
 def kill_process(pid):
     try:
@@ -11,7 +23,7 @@ def kill_process(pid):
         os.kill(int(pid), signal.SIGKILL)
 
 def running_instances(name):
-    '''returns a list of pids of running chrome processes'''
+    '''returns a list of pids of running processes with given name'''
     try:
         output = subprocess.Popen(['ps', 'aux'], stdout=subprocess.PIPE).communicate()[0]
         processes = output.split('\n')
@@ -22,4 +34,4 @@ def running_instances(name):
         return pids
 
     except subprocess.CalledProcessError:
-        return []
+        logger.error('Called Process Error', exc_info=True)
