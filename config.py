3294318c68e2803844f85e2ded12224e358c7d60
Maxwel Dsouza <maxellusionist@gmail.com>
2015-06-23 22:03:13 +0530

    fix config

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import json
 
-with open('config.json') as f:
+try:
+    f = open('config.json', 'r')
     config_text = f.read()
+except IOError:
+    print 'Check whether config.json exists'
 config = json.loads(config_text)
