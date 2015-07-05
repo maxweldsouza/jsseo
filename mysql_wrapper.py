11b4f68b4377792ddc9b7a02ea51ab0a128d2496
Maxwel Dsouza <maxellusionist@gmail.com>
2015-07-05 20:20:47 +0530

    ordering for caching pages

--- mysql_wrapper.py
+++ mysql_wrapper.py
@@ -46,19 +46,19 @@ class MySqlWrapper():
                 self.db.put('''
                 insert into page
                 (site_hostname, page_path, page_content, page_sha1,
-                page_expires, page_expiresevery) values
-                (%s, %s, %s, %s, %s, %s)
-                ''', (urlobj.origin, urlobj.path, html, hsh, expires, default_expiry_time))
+                page_expires, page_expiresevery, page_attempts) values
+                (%s, %s, %s, %s, %s, %s, %s)
+                ''', (urlobj.origin, urlobj.path, html, hsh, expires, default_expiry_time, 0))
             else:
                 pageid = current[0]
                 self.db.put('''
                 update page
                 set
                 site_hostname = %s, page_content = %s, page_sha1 = %s,
-                page_expires = %s, page_expiresevery = %s
+                page_expires = %s, page_expiresevery = %s, page_attempts = %s
                 where
                 page_id = %s
-                ''', (urlobj.origin, html, hsh, expires, default_expiry_time, pageid))
+                ''', (urlobj.origin, html, hsh, expires, default_expiry_time, pageid, 0))
         except Exception, e:
             logging.error('Could not save page', exc_info=True)
 
@@ -75,14 +75,33 @@ class MySqlWrapper():
 
     def next_url(self, hostname):
         '''gets the url of the next page to be cached. returns
-        none if there are no pages left to be cached'''
+        none if there are no pages left to be cached. the preference
+        for the order in which paths are returned is:
+            paths that are not indexed
+            paths with the earliest expiration time with no failed attempts
+            paths with the earliest expiration time with one failed attempts
+            paths with the earliest expiration time with two failed attempts
+
+            after three failed attempts dont attempt to index the page'''
         try:
             path = self.db.get_one('''
             select page_path
             from page
             where site_hostname = %s
-            and (page_expires < current_timestamp()
-            or page_content is null)''', (hostname,))
+            and page_content is null
+            ''', (hostname,))
+
+            failed_attempts = 0
+            while failed_attempts < 3 and not path:
+                path = self.db.get_one('''
+                select page_path
+                from page
+                where site_hostname = %s
+                and page_attempts = %s
+                order by page_expires
+                ''', (hostname, failed_attempts))
+                failed_attempts += 1
+
             if path:
                 return hostname + path
             else:
