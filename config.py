d396df2a9daa4a3568f8e131ea1a20166201ffd5
Maxwel Dsouza <maxellusionist@gmail.com>
2015-07-08 18:42:13 +0530

    config location relative to file path

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import json
+import os
 
 try:
-    f = open('config.json', 'r')
+    config_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config.json')
+    f = open(config_path, 'r')
     config_text = f.read()
 except IOError:
     print 'Check whether config.json exists'
